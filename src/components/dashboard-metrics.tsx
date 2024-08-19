a8716ae8d3cbc80c35cec5158741982ef3961248
Oluwademilade Ala <demiladeala@gmail.com>
2024-08-19 01:10:46 +0100

    feat: add dashboard pages

--- src/components/dashboard-metrics.tsx
+++ src/components/dashboard-metrics.tsx
@@ -15,7 +15,7 @@ const statusConfig = [
       icon: <VscOutput size={35} />,
       color: "text-[#FF5733]",
       arrow: <RxDoubleArrowDown />,
-      description: "Fresh issues"
+      description: "Fresh issus"
     },
     {
       title: "Pending",
